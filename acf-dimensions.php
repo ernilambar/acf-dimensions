08e9d10c0643dea4d6e1d44e2f0a7451e1b3c075
Nilambar Sharma <nilambar@outlook.com>
2022-09-21 16:36:33 +0545

    fix: setup

--- acf-dimensions.php
+++ acf-dimensions.php
@@ -1,13 +1,15 @@
 <?php
 /**
  * Plugin Name: ACF Dimensions
- * Plugin URI: https://github.com/ernilambar/acf-dimensions
+ * Plugin URI: https://github.com/ernilambar/acf-dimensions/
  * Description: ACF dimensions field.
- * Version: 1.0.3
+ * Version: 1.0.4
  * Author: Nilambar Sharma
  * Author URI: https://www.nilambar.net/
  * Text Domain: acf-dimensions
  * GitHub Plugin URI: ernilambar/acf-dimensions
+ * Primary Branch: main
+ * Release Asset: true
  * License: GPL-2.0+
  * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
  *
@@ -20,6 +22,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 // Define.
+define( 'ACF_DIMENSIONS_VERSION', '1.0.4' );
 define( 'ACF_DIMENSIONS_BASENAME', basename( dirname( __FILE__ ) ) );
 define( 'ACF_DIMENSIONS_DIR', rtrim( plugin_dir_path( __FILE__ ), '/' ) );
 define( 'ACF_DIMENSIONS_URL', rtrim( plugin_dir_url( __FILE__ ), '/' ) );
@@ -49,7 +52,7 @@ if ( ! class_exists( 'NS_ACF_Plugin_Dimensions' ) ) :
 		 */
 		public function __construct() {
 			$this->settings = array(
-				'version' => '1.0.3',
+				'version' => ACF_DIMENSIONS_VERSION,
 				'url'     => plugin_dir_url( __FILE__ ),
 				'path'    => plugin_dir_path( __FILE__ ),
 			);
